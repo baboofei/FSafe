61e16f6d5540214e75c1a511331b4ccc73922e58
Claude Luo <babooclaude@hotmail.com>
2015-01-13 14:36:25 -0500

    Added dynamic temperature threashold configuration

--- arduos.cpp
+++ arduos.cpp
@@ -8,6 +8,7 @@
 
 typedef struct {
 	int pin_temp;
+	int pin_potn;
 	int pin_led_r;
 	int pin_led_g;
 } hwconf_t;
@@ -17,6 +18,7 @@ hwinit (hwconf_t hw) {
 	Serial.begin(9600);
 
 	pinMode(hw.pin_temp, INPUT);
+	pinMode(hw.pin_potn, INPUT);
 	pinMode(hw.pin_led_r, OUTPUT);
 	pinMode(hw.pin_led_g, OUTPUT);
 }
@@ -29,21 +31,24 @@ hwreset (hwconf_t hw) {
 
 int
 hwchck (hwconf_t hw) {
-
-	if(hw.pin_temp == hw.pin_led_r || hw.pin_temp == hw.pin_led_g || hw.pin_led_r == hw.pin_led_g)
-		return HWCHCK_ERR_REPEATPIN;
-
-	int apins[6] = {A0, A1, A2, A3, A4, A5};
-	int i;
-	int isok = 0;
-
-	for(i = 0; i < 5; ++i)
-		if(hw.pin_temp == apins[i])
-			isok = 1;
-
-	if(!isok)
-		return HWCHCK_ERR_TEMPPINNOTANALOG;
-
+/*
+ *
+ *    if(hw.pin_temp == hw.pin_led_r || hw.pin_temp == hw.pin_led_g || hw.pin_led_r == hw.pin_led_g)
+ *        return HWCHCK_ERR_REPEATPIN;
+ *
+ *    int apins[6] = {A0, A1, A2, A3, A4, A5};
+ *    int i;
+ *    int isok = 0;
+ *
+ *    for(i = 0; i < 5; ++i)
+ *        if(hw.pin_temp == apins[i])
+ *            isok = 1;
+ *
+ *    if(!isok)
+ *        return HWCHCK_ERR_TEMPPINNOTANALOG;
+ *
+ *    return 0;
+ */
 	return 0;
 }
 
@@ -57,9 +62,15 @@ calctemp (float rawv) {
 	return 100 * rawv - 50;
 }
 
-const hwconf_t conf = {A0, 3, 4};
+float
+calcthresh (float read) {
+	#define   DYN_TEMP_LOW     0
+	#define   DYN_TEMP_HIGH   100
 
-const int conf_temp = CONF_TEMP_THREASHOLD_STATIC;
+	return (read / 1023) * (DYN_TEMP_HIGH - DYN_TEMP_LOW) + DYN_TEMP_LOW;
+}
+
+const hwconf_t conf = {A0, A1, 3, 4};
 
 void
 setup (void) {
@@ -77,9 +88,12 @@ loop (void) {
 
 	hwreset(conf);
 
-	float rawv = calcrawv(analogRead(A0), 5);
+	float rawv = calcrawv(analogRead(conf.pin_temp), 5);
 	float temp = calctemp(rawv);
 
+	float conf_temp = calcthresh(analogRead(conf.pin_potn));
+
+	Serial.println(conf_temp);
 	Serial.println(temp);
 
 	if(temp >= conf_temp || is_rotten)
